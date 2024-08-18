e0b63a2744b7c71b653ec23bc33375ccb37b4c5a
Indrani <indranisaha468@gmail.com>
2024-08-18 08:33:14 +0530

    Initial commit

---
+++ src/HW.java
@@ -0,0 +1,17 @@
+public class HW{
+    public static void main(String[] args) {
+
+    }
+}
+class Server_data{
+    public class Adddata(){
+
+    }
+    public class createTable{
+
+    }
+}
+class Student_DAO{
+    private final String url="jdbc:mysql://localhost:3306/Student";
+    private final String usname
+}
