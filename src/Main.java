dc15b37f65017381a6012118d9ba75c87f69d9ae
Indrani <indranisaha468@gmail.com>
2024-08-19 12:47:33 +0530

    add

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,17 @@ class MainClass{
     public static void main(String[] args) {
         Server_data sd=new Server_data();
         // sd.createDatabase("Student");
-        sd.createTable("stud");
+      //  sd.createTable("stud");
 //
 //        sd.deleteTable("student");
 //        sd.createTable("student");
-//          sd.addData("stud");
-        sd.search("stud");
-        sd.search("student");
-
+//          sd.addData("student");
+//        sd.search("stud");
+//        sd.showTable("student");
+//        sd.search("student");
+//        sd.numberOfStudent("stud");
+//        sd.eraseData("stud");
+        sd.changeSchema("student");
     }
 
 }
